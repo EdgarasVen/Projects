47f34bd9c745822fd137fda663570814ff80637f
EdgarasVen <55981137+EdgarasVen@users.noreply.github.com>
2019-11-19 18:15:37 +0200

    added some methods to MyInteger

--- Java/OOP/src/org/mokykla/praktika/main/Solution.java
+++ Java/OOP/src/org/mokykla/praktika/main/Solution.java
@@ -11,38 +11,8 @@ import org.mokykla.praktika.opp4.*;
 import java.util.ArrayList;
 
 public class Solution {
-
-
-
-
     public static <shape> void main(String[] args) {
-
-        ArrayList <Shape> list = new ArrayList<>();
-        Circle circle=new Circle();
-        Triangle triangle=new Triangle();
-        Square square=new Square();
-        Rectangle rectangle=new Rectangle();
-
-
-        list.add(circle);
-        list.add(triangle);
-        list.add(square);
-        list.add(rectangle);
-
-        for (Shape shape:list) {
-            System.out.println(shape.toString());
-        }
-
-        //  visu plociu suma
-        double sumOfArea=0;
-        for (Shape shape:list) {
-            sumOfArea+=((Shape)shape).getArea();
-        }
-        System.out.println("Sum of all areas is : ");
-        System.out.println(sumOfArea);
-
+        
     }
-
-
 
 }
--- Java/OOP/src/org/mokykla/praktika/opp3/MyString.java
+++ Java/OOP/src/org/mokykla/praktika/opp3/MyString.java
@@ -1,20 +1,15 @@
 package org.mokykla.praktika.opp3;
 
+import java.util.Locale;
+
 public class MyString {
     private char[] chars;
     private int length;
-        /* String sr;*/
-        /*sr.length();
-        sr.charAt(1);
-        sr.substring(1,3);
-        sr.toLowerCase();
-        sr.equals("ss");
-        sr.valueOf(1);*/
+
 
     public MyString(){
 
     }
-
     public MyString(String string){
         if (string.length()==0) return;
         else {
@@ -38,11 +33,60 @@ public class MyString {
         return count;
     }
     public static String substring(String string,int begin,int end){
-        String st = "";
+        StringBuilder st = new StringBuilder();
         char[] ch = string.toCharArray();
         for (int i = begin; i < end; i++) {
-            st+=ch[i];
+            st.append(ch[i]);
         }
-        return st;
+        return st.toString();
+    }
+    public static String substring(String string,int begin){
+        StringBuilder st = new StringBuilder();
+        char[] ch = string.toCharArray();
+        for (int i = begin; i < ch.length; i++) {
+            st.append(ch[i]);
+        }
+        return st.toString();
+    }
+    public static String toLowerCase (String string){
+        StringBuilder st = new StringBuilder();
+        char[] ch = string.toCharArray();
+        for (int i = 0; i <ch.length ; i++) {
+            int unicode=(int)ch[i];
+            if(unicode>=65 && unicode<=90)  unicode=unicode+32;
+            st.append((char)unicode);
+        }
+        return st.toString();
+    }
+    public static String toUpperCase (String string){
+        StringBuilder st = new StringBuilder();
+        char[] ch = string.toCharArray();
+        for (int i = 0; i <ch.length ; i++) {
+            int unicode=(int)ch[i];
+            if(unicode>=97 && unicode<=122)  unicode=unicode-32;
+            st.append((char)unicode);
+        }
+        return st.toString();
+    }
+    public static boolean equals(String string, String otherString){
+        if(string.length()!=otherString.length())
+            return false;
+        int sum=0,sum2=0;
+        char[] ch = string.toCharArray();
+        char[] ch2 = otherString.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            sum+=(int)ch[i];
+        }
+        for (int i = 0; i < ch2.length; i++) {
+            sum2+=(int)ch2[i];
+        }
+        return sum==sum2;
+    }
+    public static String valueOf(int i){
+       return Integer.toString(i);
+    }
+    public static boolean compare(String string,String otherString){
+
+        return false;
     }
 }
