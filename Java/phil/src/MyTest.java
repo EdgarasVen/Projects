3864f459b7e827479c0b759f0a97db9bc7965409
EdgarasVen <edgarasvenzlauskas@gmail.com>
2020-01-21 18:54:42 +0200

    remove

--- Java/phil/src/MyTest.java
+++
@@ -1,8 +0,0 @@
-import philatelist.Philatelist;
-import philatelist.PhilatelistBaseTest;
-
-public class MyTest {
-    public static void main(String[] args) {
-        System.out.println(" sad ");
-    }
-}
