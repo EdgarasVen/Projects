20a12d0316b91a4fbb077d8c7b3a88b1d902dcbf
EdgarasVen <55981137+EdgarasVen@users.noreply.github.com>
2020-03-17 14:39:44 +0200

    initial commit

--- Java/manager/src/test/java/MyTestClass.java
+++
@@ -1,8 +0,0 @@
-import lt.itakademija.exam.ProjectManager;
-import lt.itakademija.exam.test.BaseTest;
-
-public class MyTestClass extends BaseTest {
-    protected ProjectManager createProjectTracker() {
-        return new MyProjectManager();
-    }
-}
