0385c99fa2b6f610b48bbb1b8ffb88f53f0693e7
EdgarasVen <55981137+EdgarasVen@users.noreply.github.com>
2020-05-19 12:07:27 +0300

    organizer done

--- Java/kontrolinis/documents-organizer/src/main/java/Organizer.java
+++ Java/kontrolinis/documents-organizer/src/main/java/Organizer.java
@@ -22,29 +22,28 @@ public class Organizer implements  DocumentOrganizer {
     public void organize(DocumentProducer documentProducer, DocumentConsumer documentConsumer) {
         if(documentConsumer==null)throw new IllegalArgumentException();
         if(documentProducer==null)throw new IllegalArgumentException();
-        while (documentProducer.get()!=null){
-            Document document= documentProducer.get();
+        boolean flag = true;
+        while (flag){
+            Document document=documentProducer.get();
             if (document!=null){
-                System.out.println(detector.detect(document));
                 lineCount+=document.getLines().size();
+                count++;
                 if (detector.detect(document)==DocumentType.IMPORTANT) {
                     documentConsumer.consumeImportant(document);
-                    count++;
                 }
                 if (detector.detect(document)==DocumentType.ORDINARY){
                     documentConsumer.consumeOrdinary(document);
-                    count++;
                 }
                 if (detector.detect(document)==DocumentType.SPAM){
                     documentConsumer.consumeSpam(document);
-                    count++;
                 }
                 if (detector.detect(document)==DocumentType.UNKNOWN){
-                    count++;
                     throw new UnknownDocumentTypeException(document.getTitle());
-
                 }
-        }
+
+            }
+            else
+                flag=false;
         }
     }
 }
